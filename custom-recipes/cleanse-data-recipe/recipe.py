95177db177a3fc574a1bd9d94b6a73e5916588d3
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 21:54:58 +0000

    Edited file 'custom-recipes/cleanse-data-recipe/recipe.py'

--- custom-recipes/cleanse-data-recipe/recipe.py
+++ custom-recipes/cleanse-data-recipe/recipe.py
@@ -1,3 +1,9 @@
 from k8s_monitoring.cleanse_data import main
-
 from datetime import datetime
+
+
+dt = datetime.utcnow()
+r = main.cleanse_data(dt, "incoming|cluster_data")
+r = main.cleanse_data(dt, "incoming|nodegroup_data")
+r = main.cleanse_data(dt, "incoming|pod_status")
+r = main.cleanse_data(dt, "incoming|nodegroup_status")
