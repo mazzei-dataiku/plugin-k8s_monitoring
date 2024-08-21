495b38fbd32c02fcdb1f098895c8e1f16de5bfc5
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 15:25:46 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/main.py'

--- python-lib/k8s_monitoring/poll_data/main.py
+++ python-lib/k8s_monitoring/poll_data/main.py
@@ -1,21 +1,6 @@
-import pandas as pd
-import datetime
-from kubernetes import client as k8s_client
-from kubernetes import config as k8s_config
+from k8s_monitoring import config
 
 def poll_data():
-    # Load config variables
-    kube_config = "/data/dataiku/dss_data/clusters/fe-sandbox-cluster/exec/kube_config"
-    try:
-        k8s_config.load_kube_config(config_file=kube_config)
-    except Exception as e:
-        return e
-    
-    v1 = k8s_client.CoreV1Api()
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    l =[]
 
-    for i in ret.items:
-        break
         
-    return i
+    return config.data_dir
