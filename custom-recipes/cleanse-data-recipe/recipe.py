e9e23acc4caccb47aee67104233641fc8af11fe2
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 21:53:29 +0000

    Edited file 'custom-recipes/cleanse-data-recipe/recipe.py'

--- custom-recipes/cleanse-data-recipe/recipe.py
+++ custom-recipes/cleanse-data-recipe/recipe.py
@@ -1,12 +1,3 @@
-import dataiku
-from dataiku.customrecipe import get_input_names_for_role
-from dataiku.customrecipe import get_output_names_for_role
-from dataiku.customrecipe import get_recipe_config
+from k8s_monitoring.cleanse_data import main
 
-input_A_names = get_input_names_for_role('input_A_role')
-input_A_datasets = [dataiku.Dataset(name) for name in input_A_names]
-
-output_A_names = get_output_names_for_role('main_output')
-output_A_datasets = [dataiku.Dataset(name) for name in output_A_names]
-my_variable = get_recipe_config()['parameter_name']
-my_variable = get_recipe_config().get('parameter_name', None)
+from datetime import datetime
