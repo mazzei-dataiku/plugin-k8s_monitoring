a1e3be11cc19e53f40bf10a79b4917bd6f4ec65b
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 19:32:03 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/polling/nodegroup_data.py'

--- python-lib/k8s_monitoring/poll_data/polling/nodegroup_data.py
+++ python-lib/k8s_monitoring/poll_data/polling/nodegroup_data.py
@@ -1,2 +1,18 @@
-def ead():
-    return
+from k8s_monitoring.poll_data import config
+
+def get_data(dt):
+    cluster_type = config.cluster_type
+    
+    if cluster_type == "AWS":
+        from k8s_monitoring.poll_data.cloud_provider import aws_cluster_data
+        df = aws_cluster_data.get_data(dt)
+        return df
+    
+    elif cluster_type == "Azure":
+        return 123
+    
+    elif cluster_type == "GCP":
+        return 123
+    
+    else:
+        raise Exception(f"Invalid cluster type: {cluster_type}")
