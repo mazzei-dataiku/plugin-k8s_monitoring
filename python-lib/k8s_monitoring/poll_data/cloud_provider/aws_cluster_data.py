954f316e1a6b392d95f978ed7d1fba794a464ae2
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 17:54:32 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/cloud_provider/aws_cluster_data.py'

--- python-lib/k8s_monitoring/poll_data/cloud_provider/aws_cluster_data.py
+++ python-lib/k8s_monitoring/poll_data/cloud_provider/aws_cluster_data.py
@@ -1,50 +1,27 @@
-from .helper import load_yaml
-
 import pandas as pd
 import boto3
 
 
-def get_data(NOW, instance_name):
-    # Build Cluster Data Frame
-    k8s_yaml = load_yaml(instance_name)
-    cluster_name = k8s_yaml['metadata']['name']
-    
-    eks = boto3.client("eks")
+def get_data(dt, cluster_name, aws_region):
+    eks = boto3.client("eks", region_name=aws_region)
     r = eks.describe_cluster(name=cluster_name)
-    cluster_create_date = r['cluster']['createdAt'],
-    cluster_version = r['cluster']['version'],
-    cluster_platform_version = r['cluster']['platformVersion']
-
-    c = [
-        [
-            NOW,
-            cluster_name,
-            cluster_create_date[0],
-            cluster_version[0],
-            cluster_platform_version
-        ]
+    data = [
+            dt, aws_region, cluster_name,
+            r['cluster']['createdAt'],
+            r["cluster"]["status"],
+            r['cluster']['version'],
+            r['cluster']['platformVersion']
     ]
 
     columns = [
         'date_time',
+        'aws_region',
         'cluster_name',
         'cluster_create_date',
+        'cluster_status',
         'cluster_version',
         'cluster_plat_version'
     ]
-    clusters_df = pd.DataFrame(c, columns=columns)
-    
-    # Forcing some DT stuff
-    clusters_df['date_time'] = clusters_df['date_time'].dt.tz_localize(None)
-    clusters_df['date_time'] = clusters_df['date_time'].dt.tz_localize('UTC')
-
-    clusters_df['cluster_create_date'] = clusters_df['cluster_create_date'].dt.tz_localize(None)
-    clusters_df['cluster_create_date'] = clusters_df['cluster_create_date'].dt.tz_localize('UTC')
-    
-    # Forcing some other items
-    c = list(clusters_df.columns)
-    c.remove('date_time')
-    c.remove('cluster_create_date')
-    clusters_df[c] = clusters_df[c].astype('str')
+    df = pd.DataFrame(c, columns=columns)
 
     return clusters_df
