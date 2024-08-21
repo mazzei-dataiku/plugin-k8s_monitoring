27159259e055bdc1e69b10eff5878323385b8b1e
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 19:28:48 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/cloud_provider/aws_nodegroup_data.py'

--- python-lib/k8s_monitoring/poll_data/cloud_provider/aws_nodegroup_data.py
+++ python-lib/k8s_monitoring/poll_data/cloud_provider/aws_nodegroup_data.py
@@ -5,10 +5,12 @@ import boto3
 
 
 def get_data(dt):
-    aws_region = 'us-west-2'
-    cluster_name = 'fe-sandbox-cluster'
+    aws_region = config.aws_region
+    cluster_name = config.clsuter_name
+    
     eks = boto3.client("eks", region_name=aws_region)
     ec2 = boto3.client("ec2", region_name=aws_region)
+    
     data = []
     r = eks.list_nodegroups(clusterName=cluster_name)
     for ng in r['nodegroups']:
@@ -16,6 +18,7 @@ def get_data(dt):
             r = eks.describe_nodegroup(clusterName=cluster_name, nodegroupName=ng)
         except:
             continue
+    
         date_time = dt
         nodegroup_name = ng
         nodegroup_instance_type = r['nodegroup']['instanceTypes']
@@ -27,6 +30,7 @@ def get_data(dt):
         instance_desc = r['nodegroup']['amiType']
         instance_cores = instance_type_md['InstanceTypes'][0]['VCpuInfo']['DefaultCores']
         instance_memory = instance_type_md['InstanceTypes'][0]['MemoryInfo']['SizeInMiB']
+        
         try:
             gpu_count = instance_type_md["InstanceTypes"][0]["GpuInfo"]["Gpus"][0]["Count"]
             gpu_cpu = instance_type_md["InstanceTypes"][0]["GpuInfo"]["Gpus"][0]["Count"]
@@ -36,8 +40,10 @@ def get_data(dt):
             gpu_cpu = 0
             gpu_memory = 0
         instance_price = 1.0080
+        
         if gpu_count > 0:
             instance_price = 0.5260
+        
         t = [
             dt,
             nodegroup_name, nodegroup_instance_type,
@@ -45,7 +51,10 @@ def get_data(dt):
             instance_desc, instance_cores, instance_memory, gpu_count, gpu_cpu, gpu_memory,
             instance_price
         ]
+        
         data.append(t)
+    
+    
     cols = [
         "date_time",
         "nodegroup_name", "nodegroup_instance_type",
