e89f4f0e4b8f2d00e1a58149abe7408b4a8a2cbc
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 21:10:22 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/main.py'

--- python-lib/k8s_monitoring/poll_data/main.py
+++ python-lib/k8s_monitoring/poll_data/main.py
@@ -11,26 +11,26 @@ def poll_data():
     # Load config variables
     dt = config.dt
     folder_name = config.raw_folder_name
-    folder_type = config.raw_folder_type
+    folder_path = config.raw_folder_path
     
     # Cluster Information
     name = "cluster_data"
     cluster_df = cluster_data.get_data(dt)
-    helper.save_data_folder(dt, name, cluster_df, folder_name, folder_type)
+    helper.save_data_folder(dt, name, cluster_df, folder_name, folder_path)
      
     # Nodegroup Information
     name = "nodegroup_data"
     nodegroup_df = nodegroup_data.get_data(dt)
-    helper.save_data_folder(dt, name, nodegroup_df, folder_name, folder_type)
+    helper.save_data_folder(dt, name, nodegroup_df, folder_name, folder_path)
     
     # Pod Status
     name = "pod_status"
     pods_df = pod_status.get_data(dt)
-    helper.save_data_folder(dt, name, pods_df, folder_name, folder_type)   
+    helper.save_data_folder(dt, name, pods_df, folder_name, folder_path)   
     
     # Node Status
     name = "nodegroup_status"
     ngs_df = nodegroup_status.get_data(dt, pods_df)
-    helper.save_data_folder(dt, name, ngs_df, folder_name, folder_type)
+    helper.save_data_folder(dt, name, ngs_df, folder_name, folder_path)
     
     return "FINISH"
