0ab1fb1393eb1d340240c08ebe3fccd70b538f8e
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 15:26:36 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/main.py'

--- python-lib/k8s_monitoring/poll_data/main.py
+++ python-lib/k8s_monitoring/poll_data/main.py
@@ -1,4 +1,14 @@
 from k8s_monitoring import config
 
 def poll_data():
+    # Load config variables
+    dt = config.dt
+    data_dir = config.data_dir
+    project_id = config.project_id
+    zone = config.zone
+    cluster = config.cluster
+    
+    # mode type
+    mode = "incoming"
+    
     return config.data_dir
