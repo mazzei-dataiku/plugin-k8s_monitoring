62cacfd0edb6fdf29f6465e54cb83f7647bfae77
smazzei <stephen.mazzei@dataiku.com>
2024-08-21 15:19:12 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/main.py'

--- python-lib/k8s_monitoring/poll_data/main.py
+++ python-lib/k8s_monitoring/poll_data/main.py
@@ -5,7 +5,7 @@ from kubernetes import config as k8s_config
 
 def poll_data():
     # Load config variables
-    p = "/data/dataiku/dss_data/clusters/fe-sandbox-cluster/exec/kube_config"
+    kube_config = "/data/dataiku/dss_data/clusters/fe-sandbox-cluster/exec/kube_config"
     try:
         k8s_config.load_kube_config(config_file=kube_config)
     except Exception as e:
