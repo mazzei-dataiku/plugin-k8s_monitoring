3461d967538118fcf1cf74f795b7b380c9fe5c28
smazzei <stephen.mazzei@dataiku.com>
2024-08-22 15:01:06 +0000

    Edited file 'custom-recipes/cleanse-data-recipe/recipe.py'

--- custom-recipes/cleanse-data-recipe/recipe.py
+++ custom-recipes/cleanse-data-recipe/recipe.py
@@ -1,6 +1,10 @@
 from k8s_monitoring.cleanse_data import main
 from datetime import datetime
 
+input_A_names = get_input_names_for_role('input_folder')
+print(f"AHHHHHHHH input_A_names")
+
+
 
 dt = datetime.utcnow()
 r = main.cleanse_data(dt, "incoming|cluster_data")
