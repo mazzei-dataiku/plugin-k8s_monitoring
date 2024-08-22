448abea799d1eabb2f30e1ea700bd9ac5e4640fa
admin <Stephen.Mazzei@dataiku.com>
2024-08-22 17:04:14 +0000

    Edited file 'python-lib/k8s_monitoring/poll_data/polling/cluster_data.py'

--- python-lib/k8s_monitoring/poll_data/polling/cluster_data.py
+++ python-lib/k8s_monitoring/poll_data/polling/cluster_data.py
@@ -9,10 +9,12 @@ def get_data(self, dt):
         return df
     
     elif cluster_type == "Azure":
-        return 123
+        raise Exception("Azure cluster has not been implemented yet.")
     
     elif cluster_type == "GCP":
-        return 123
+        rom k8s_monitoring.poll_data.cloud_provider import aws_cluster_data
+        df = aws_cluster_data.get_data(self, dt)
+        return df
     
     else:
         raise Exception(f"Invalid cluster type: {cluster_type}")
