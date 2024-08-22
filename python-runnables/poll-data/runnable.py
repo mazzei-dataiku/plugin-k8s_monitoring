020458d404af275bd8ef0f3af07c5fdc6cf51e50
smazzei <stephen.mazzei@dataiku.com>
2024-08-22 14:32:42 +0000

    Edited file 'python-runnables/poll-data/runnable.py'

--- python-runnables/poll-data/runnable.py
+++ python-runnables/poll-data/runnable.py
@@ -3,26 +3,16 @@ from k8s_monitoring.poll_data import main
 
 
 class MyRunnable(Runnable):
-    """The base interface for a Python runnable"""
-
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
         
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
+        return self.config.keys()
         output = main.poll_data()
         return output
         
