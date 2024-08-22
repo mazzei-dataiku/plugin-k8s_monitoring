925b0d07cdf0779717685cc53e80624281cfb960
smazzei <stephen.mazzei@dataiku.com>
2024-08-22 15:33:12 +0000

    Edited file 'python-lib/k8s_monitoring/helper.py'

--- python-lib/k8s_monitoring/helper.py
+++ python-lib/k8s_monitoring/helper.py
@@ -28,7 +28,7 @@ def save_data_folder(self, dt, name, df):
     # setup paths
     if config.raw_folder_path == "incoming":
         dt_str = dt.strftime("%Y%m%d")
-        path   = f'/{folder_path}/{name}/{dt_year}/{dt_month}/{dt_day}/run_{dt_str}.{save_type}'
+        path   = f'/{raw_folder_path}/{name}/{dt_year}/{dt_month}/{dt_day}/run_{dt_str}.{save_type}'
     
     # Get folder
     folder = get_folder(self)
