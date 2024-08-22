0258d062e24c6d423aaa20efeae7b3948d3ebfeb
smazzei <stephen.mazzei@dataiku.com>
2024-08-22 15:24:19 +0000

    Edited file 'custom-recipes/cleanse-data-recipe/recipe.py'

--- custom-recipes/cleanse-data-recipe/recipe.py
+++ custom-recipes/cleanse-data-recipe/recipe.py
@@ -70,9 +70,6 @@ dt = datetime.utcnow()
 input_folder = get_input_names_for_role('input_folder')[0]
 output_folder = get_output_names_for_role('output_folder')[0]
 
-print(f"AHHHHHHHHH {input_folder}")
-print(f"AHHHHHHHHH {output_folder}")
-
 # Cleanse Data
 r = cleanse_data(dt, input_folder, output_folder, "incoming|cluster_data")
 r = cleanse_data(dt, input_folder, output_folder, "incoming|nodegroup_data")
